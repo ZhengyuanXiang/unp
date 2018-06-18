c6c8926818e71cfcd83d58cd99025676d1d0947d
ZhengyuanXiang <xiang@ZhengyuanXiangs-MacBook-Pro.local>
2018-06-18 23:34:02 +0800

    add socket wrap

--- tcpcliserv/tcpserv01.c
+++ tcpcliserv/tcpserv01.c
@@ -7,20 +7,20 @@ int main(int argc, char **argv)
     socklen_t chilen;
 
     struct sockaddr_in cliaddr, servaddr;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(8000);
     
-    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-    listen(listenfd, 5);
+    Listen(listenfd, 5);
     for (;;)
     {
         chilen = sizeof(cliaddr);
-        connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &chilen);
+        connfd = Accept(listenfd, (struct sockaddr*)&cliaddr, &chilen);
         if ((childpip = fork()) == 0)
         {
             close(listenfd);
